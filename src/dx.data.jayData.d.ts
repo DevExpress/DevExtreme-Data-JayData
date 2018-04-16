9596bbb205241dcab7d07933b9f9a84470315179
Alexander Ziborov <San4es@users.noreply.github.com>
2018-04-16 14:34:55 +0300

    Add TS definitions

---
+++ src/dx.data.jayData.d.ts
@@ -0,0 +1,16 @@
+﻿/// <reference path="../data/data.store.d.ts" />  
+/// <reference path="../jaydata.d.ts" />
+declare module DevExpress.data {
+    export interface JayDataStoreOptions extends StoreOptions {
+        autoCommit?: boolean;
+        queryable: $data.Queryable<$data.Entity>;
+    }
+    export class JayDataStore extends Store {
+        constructor(queryable: $data.Queryable<$data.Entity>);
+        constructor(options: JayDataStoreOptions);
+
+        public queryable(): $data.Queryable<$data.Entity>;
+        public entityType(): string;
+        public entityContext(): $data.EntityContext;
+    }
+}
